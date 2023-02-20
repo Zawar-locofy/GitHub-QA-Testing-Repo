c8ccac60c288bf5affa08ec6386a719f92fe9f61
Zawar-locofy <zawar@locofy.ai>
2023-02-20 11:29:49 +0000

    Locofy commit

---
+++ src/pages/ServiceResponsive.tsx
@@ -0,0 +1,288 @@
+import { FunctionComponent } from "react";
+import styles from "../components/ServiceResponsive.module.css";
+
+const ServiceResponsive: FunctionComponent = () => {
+  return (
+    <div className={styles.serviceResponsive}>
+      <div className={styles.banner}>
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <b className={styles.contactUs}>Contact Us</b>
+            <div className={styles.line} />
+          </div>
+          <div className={styles.thereAreMany}>
+            There are many variations of passages of Lorem Ipsum available, have
+            suffered alteration in some form.
+          </div>
+        </div>
+        <img
+          className={styles.cogaiLogotrans11Icon}
+          alt=""
+          src="../../public/cogailogotrans1-1@2x.png"
+        />
+        <div className={styles.homeParent}>
+          <div className={styles.home}>Home</div>
+          <div className={styles.about}>About</div>
+          <div className={styles.about}>Services</div>
+          <div className={styles.about}>Industries</div>
+          <div className={styles.about}>Resources</div>
+        </div>
+        <div className={styles.rectangleParent}>
+          <div className={styles.groupChild} />
+          <div className={styles.getAQuote}>GET A QUOTE</div>
+        </div>
+      </div>
+      <div className={styles.banner1}>
+        <div className={styles.imageParent}>
+          <div className={styles.banner}>
+            <div className={styles.background} />
+          </div>
+          <div className={styles.content1}>
+            <div className={styles.title1}>
+              <b className={styles.contactUs}>Services</b>
+            </div>
+          </div>
+          <img
+            className={styles.cogaiLogotrans11Icon1}
+            alt=""
+            src="../../public/cogailogotrans1-1@2x.png"
+          />
+          <div className={styles.homeGroup}>
+            <div className={styles.about}>Home</div>
+            <div className={styles.about}>About</div>
+            <div className={styles.services2}>Services</div>
+            <div className={styles.about}>Industries</div>
+            <div className={styles.about}>Resources</div>
+          </div>
+          <div className={styles.rectangleGroup}>
+            <div className={styles.groupChild} />
+            <div className={styles.getAQuote}>GET A QUOTE</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.groupParent}>
+        <div className={styles.rectangleContainer}>
+          <div className={styles.groupInner} />
+          <div className={styles.copyright2022}>
+            Copyright @ 2022. All Rights Reserved.
+          </div>
+          <b className={styles.otherLinks}>Other Links</b>
+          <div className={styles.termsCondition}>{`Terms & Condition`}</div>
+          <div className={styles.privacyPolicy}>Privacy Policy</div>
+          <div className={styles.reportAbuse}>Report Abuse</div>
+          <div className={styles.contactUs1}>Contact Us</div>
+          <b className={styles.serives}>Serives</b>
+          <div className={styles.applicationDevelopment}>
+            Application Development
+          </div>
+          <div className={styles.humanToMachineCommunication}>
+            Human-to-Machine Communication
+          </div>
+          <div className={styles.machineToMachineCommunicati}>
+            Machine-to-Machine Communication
+          </div>
+          <div className={styles.videoAnalytics}>Video Analytics</div>
+          <div className={styles.div} />
+          <img
+            className={styles.groupIcon}
+            alt=""
+            src="../../public/group-215.svg"
+          />
+          <img
+            className={styles.frameIcon}
+            alt=""
+            src="../../public/frame.svg"
+          />
+          <div className={styles.weSpecializeIn}>
+            We specialize in developing custom applications that streamline
+            processes and improve efficiency for businesses of all sizes.
+          </div>
+        </div>
+        <div className={styles.contactUsParent}>
+          <b className={styles.infocogaiuk}>CONTACT US</b>
+          <div className={styles.frameParent}>
+            <div className={styles.vectorParent}>
+              <img
+                className={styles.vectorIcon}
+                alt=""
+                src="../../public/vector.svg"
+              />
+              <div className={styles.infocogaiuk}>+44-(0)-1474-338-718</div>
+            </div>
+            <div className={styles.vectorGroup}>
+              <img
+                className={styles.vectorIcon1}
+                alt=""
+                src="../../public/vector1.svg"
+              />
+              <div className={styles.infocogaiuk}>info@cogai.uk</div>
+            </div>
+            <div className={styles.groupGroup}>
+              <img
+                className={styles.groupIcon1}
+                alt=""
+                src="../../public/group.svg"
+              />
+              <div className={styles.infocogaiuk}>
+                <p
+                  className={styles.hazelGroveStockport}
+                >{`Cogai LTD, 6 Station View, `}</p>
+                <p
+                  className={styles.hazelGroveStockport}
+                >{`Hazel Grove, Stockport, `}</p>
+                <p className={styles.englandSk75er}>England, SK7 5ER</p>
+              </div>
+            </div>
+            <div className={styles.vectorContainer}>
+              <img
+                className={styles.vectorIcon2}
+                alt=""
+                src="../../public/vector2.svg"
+              />
+              <div className={styles.infocogaiuk}>Monday To Friday</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.groupContainer}>
+        <div className={styles.groupDiv}>
+          <img
+            className={styles.rectangleIcon}
+            alt=""
+            src="../../public/rectangle-9@2x.png"
+          />
+          <div className={styles.groupParent1}>
+            <div className={styles.applicationDevelopmentIsAtParent}>
+              <div className={styles.applicationDevelopmentIsContainer}>
+                <p className={styles.hazelGroveStockport}>
+                  Application development is at the heart of what we do. Our
+                  team are skilled in creating custom software solutions that
+                  meet the unique needs of our clients. So whether you need a
+                  simple
+                </p>
+                <p className={styles.englandSk75er}>
+                  application to automate a business process or a complex system
+                  to manage your entire operation, we have the expertise and
+                  experience to deliver results.
+                </p>
+              </div>
+              <div className={styles.applicationDevelopment1}>
+                1. Application Development
+              </div>
+            </div>
+            <div className={styles.rectangleDiv} />
+          </div>
+          <div className={styles.readMore}>Read more</div>
+        </div>
+        <div className={styles.frameGroup}>
+          <div className={styles.frameDiv}>
+            <div className={styles.groupWrapper}>
+              <div className={styles.humanToMachineCommunicationParent}>
+                <div className={styles.humanToMachineCommunication1}>
+                  <p className={styles.hazelGroveStockport}>
+                    Human-to-machine communication is another crucial area of
+                    focus for our company. With the proliferation of smart
+                    devices and the Internet of Things (IoT), it's more
+                    important than ever for
+                  </p>
+                  <p className={styles.englandSk75er}>
+                    businesses to connect and communicate seamlessly with their
+                    customers and employees. Our team can help you develop the
+                    systems and technologies needed to enable effective
+                    human-to-machine communication, including wireless
+                    controls....
+                  </p>
+                </div>
+                <div className={styles.applicationDevelopment1}>
+                  2. Human-To-Machine Communication
+                </div>
+              </div>
+            </div>
+            <div className={styles.rectangleParent1}>
+              <div className={styles.groupChild1} />
+              <div className={styles.readMore1}>Read more</div>
+            </div>
+          </div>
+          <img
+            className={styles.frameChild}
+            alt=""
+            src="../../public/rectangle-11@2x.png"
+          />
+        </div>
+        <div className={styles.rectangleParent2}>
+          <img
+            className={styles.frameChild}
+            alt=""
+            src="../../public/rectangle-111@2x.png"
+          />
+          <div className={styles.groupParent2}>
+            <div className={styles.groupParent3}>
+              <div className={styles.machineToMachineCommunicatiParent}>
+                <div className={styles.machineToMachineCommunicati1}>
+                  <p className={styles.hazelGroveStockport}>
+                    Machine-to-machine communication is also an essential part
+                    of our offering. With the ability to connect and communicate
+                    with other devices and systems, businesses can automate
+                    processes,
+                  </p>
+                  <p className={styles.englandSk75er}>
+                    improve efficiency, and gather valuable data to inform
+                    decision-making. We can help you develop the infrastructure
+                    and systems needed to enable effective machine-to-machine
+                    communication and leverage the power of the IoT to drive
+                    business growth.
+                  </p>
+                </div>
+                <div className={styles.applicationDevelopment1}>
+                  3. Machine-To-Machine Communication
+                </div>
+              </div>
+              <div className={styles.rectangleDiv} />
+            </div>
+            <div className={styles.readMore2}>Read more</div>
+          </div>
+        </div>
+        <div className={styles.rectangleParent3}>
+          <img
+            className={styles.groupChild3}
+            alt=""
+            src="../../public/rectangle-10@2x.png"
+          />
+          <div className={styles.groupParent4}>
+            <div className={styles.ourVideoAnalyticsServicesAParent}>
+              <div className={styles.ourVideoAnalyticsContainer}>
+                <p className={styles.hazelGroveStockport}>
+                  Our video analytics services are designed to help businesses
+                  extract valuable insights and information from video footage.
+                  Whether you need to monitor customer behavior in a retail
+                </p>
+                <p className={styles.englandSk75er}>
+                  environment, optimize your manufacturing processes, or gather
+                  intelligence for security, our team can help you develop the
+                  technology and capabilities needed to turn video data into
+                  actionable insights.
+                </p>
+              </div>
+              <div className={styles.applicationDevelopment1}>
+                4. Video Analytics
+              </div>
+            </div>
+            <div className={styles.rectangleDiv} />
+          </div>
+          <div className={styles.readMore3}>Read more</div>
+        </div>
+        <div className={styles.groupFrame}>
+          <div className={styles.servicesWeAreProvidingWrapper}>
+            <div className={styles.servicesWeAre}>
+              Services We Are Providing
+            </div>
+          </div>
+        </div>
+        <div className={styles.homeServices}>Home / Services</div>
+        <div className={styles.frameInner} />
+      </div>
+    </div>
+  );
+};
+
+export default ServiceResponsive;
