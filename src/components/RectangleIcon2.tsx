7bc99584a46bd76d2374d6fb2242eec00d43d633
Zarttash Zafar <contact.zarttash@gmail.com>
2023-02-22 10:19:27 +0500

    locofy deleted

--- src/components/RectangleIcon2.tsx
+++
@@ -1,8 +0,0 @@
-import { FunctionComponent } from "react";
-import "./RectangleIcon2.css";
-
-const RectangleIcon2: FunctionComponent = () => {
-  return <img className="frame-inner" alt="" src="/rectangle-11@2x.png" />;
-};
-
-export default RectangleIcon2;
