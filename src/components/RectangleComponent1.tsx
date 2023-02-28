6a91919f5ca2672372de23fd1a2b195e5912584f
Zawar Mehdi <zawar@locofy.ai>
2023-02-28 19:24:00 +0500

    locofy deleted

--- src/components/RectangleComponent1.tsx
+++
@@ -1,9 +0,0 @@
-import { FunctionComponent } from "react";
-
-const RectangleComponent1: FunctionComponent = () => {
-  return (
-    <div className="absolute top-[0px] left-[2px] bg-mintcream w-[349px] h-[349px]" />
-  );
-};
-
-export default RectangleComponent1;
