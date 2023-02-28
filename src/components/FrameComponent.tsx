b09be8b8767ec1d508152dc42214dd3438773b50
Zawar Mehdi <zawar@locofy.ai>
2023-02-28 19:23:59 +0500

    locofy deleted

--- src/components/FrameComponent.tsx
+++
@@ -1,9 +0,0 @@
-import { FunctionComponent } from "react";
-
-const FrameComponent: FunctionComponent = () => {
-  return (
-    <div className="absolute top-[706px] left-[827px] w-[464px] h-[617px] overflow-hidden" />
-  );
-};
-
-export default FrameComponent;
